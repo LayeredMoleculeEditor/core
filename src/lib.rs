e6e9a02e777b8c8e857a21e7ba72621867a74a64
Hexiang Qi <qihexiang@outlook.com>
2023-10-30 17:46:13 +0800

    new version of AtomFillLayer

--- src/lib.rs
+++ src/lib.rs
@@ -1,265 +1,86 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod core;
 
-pub mod layer {
-    use rayon::prelude::*;
-    use std::{collections::{HashMap, HashSet}, hash::Hash};
+pub mod utils {
+    use std::{
+        borrow::Borrow,
+        collections::{HashMap, HashSet},
+        fmt::Debug,
+        hash::Hash,
+    };
 
-    use nalgebra::{Point3, Vector3};
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    pub struct UniqueValueMap<K: Hash + Eq + Clone, V: Hash + Eq + Clone> {
+        key_map: HashMap<K, V>,
+        validate: HashSet<V>,
+    }
 
-    pub struct AtomBuilder(isize, Point3<f64>, Option<String>, HashSet<String>);
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    pub enum InsertResult<K, V> {
+        Updated(V),
+        Duplicated(K),
+        Created,
+    }
 
-    impl AtomBuilder {
+    impl<K: Clone + Hash + Eq, V: Clone + Hash + Eq> UniqueValueMap<K, V> {
         pub fn new() -> Self {
-            AtomBuilder(0, Point3::new(0., 0., 0.), None, HashSet::new())
-        }
-
-        pub fn set_element(mut self, element: isize) -> Self {
-            self.0 = element;
-            self
-        }
-
-        pub fn set_position(mut self, position: Point3<f64>) -> Self {
-            self.1 = position;
-            self
-        }
-
-        pub fn move_position(mut self, vector: Vector3<f64>) -> Self {
-            self.1 = self.1 + vector;
-            self
-        }
-
-        pub fn set_id(mut self, id: String) -> Self {
-            self.2 = Some(id);
-            self
-        }
-
-        pub fn remove_id(mut self) -> Self {
-            self.2 = None;
-            self
-        }
-
-        pub fn add_class(mut self, class_name: String) -> Self {
-            self.3.insert(class_name);
-            self
-        }
-
-        pub fn remove_class(mut self, class_name: &str) -> Self {
-            self.3.remove(class_name);
-            self
-        }
-
-        pub fn build(&self) -> Atom {
-            Atom(self.0, self.1, self.2.clone(), self.3.clone())
-        }
-    }
-
-    pub struct Atom(isize, Point3<f64>, Option<String>, HashSet<String>);
-
-    impl Atom {
-        pub fn modify(&self) -> AtomBuilder {
-            AtomBuilder(self.0, self.1, self.2.clone(), self.3.clone())
-        }
-    }
-
-    impl Default for AtomBuilder {
-        fn default() -> Self {
-            AtomBuilder::new()
-        }
-    }
-
-    impl Default for Atom {
-        fn default() -> Self {
-            AtomBuilder::default().build()
-        }
-    }
-
-    #[derive(Clone, Copy, Debug, Eq)]
-    pub struct Pair<T>(T, T);
-    impl<T> Pair<T> {
-        pub fn new(a: T, b: T) -> Self {
-            Self(a, b)
-        }
-    }
-
-    impl<T: PartialEq + Eq> PartialEq for Pair<T> {
-        fn eq(&self, other: &Self) -> bool {
-            (self.0 == other.0 && self.1 == other.1) || (self.1 == other.0 && self.0 == other.1)
-        }
-    }
-
-    impl<T: Hash + Ord> Hash for Pair<T> {
-        fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-            let Self(a, b) = self;
-            let mut values = [a, b];
-            values.sort();
-            for value in values {
-                value.hash(state)
+            Self {
+                key_map: HashMap::new(),
+                validate: HashSet::new(),
             }
         }
-    }
 
-    pub trait ReadableAtomLayer {
-        fn get_idxs(&self) -> HashSet<usize>;
-        fn get_ids(&self) -> HashSet<String>;
-        fn get_classes(&self) -> HashSet<String>;
-        fn find_with_id(&self, target_id: &str) -> Option<usize>;
-        fn find_with_classes(&self, target_classes: &HashSet<String>) -> HashSet<usize>;
-        fn get_atom(&self, idx: usize) -> Option<&Atom>;
-    }
-
-    pub trait WritableAtomLayer {
-        fn set_atom(&mut self, idx: usize, atom: Atom) -> Option<Atom>;
-    }
-
-    pub trait ReadableAtomLayerHelper: ReadableAtomLayer {
-        fn max_idx(&self) -> Option<usize> {
-            self.get_idxs().into_iter().max()
+        pub fn data(&self) -> &HashMap<K, V> {
+            &self.key_map
         }
 
-        fn next_avaliable_idx(&self) -> usize {
-            self.max_idx().map(|v| v + 1).unwrap_or(0)
-        }
-    }
-
-    pub trait WritableAtomLayerHelper: ReadableAtomLayerHelper + WritableAtomLayer {
-        fn add_atom(&mut self, atom: Atom) -> usize {
-            let idx = self.next_avaliable_idx();
-            self.set_atom(idx, atom).unwrap();
-            idx
-        }
-    }
-
-    pub struct AtomFillLayer(HashMap<usize, Atom>);
-
-    impl AtomFillLayer {
-        fn data(&self) -> &HashMap<usize, Atom> {
-            &self.0
-        }
-
-        fn data_mut(&mut self) -> &mut HashMap<usize, Atom> {
-            &mut self.0
-        }
-    }
-
-    impl ReadableAtomLayer for AtomFillLayer {
-        fn get_idxs(&self) -> HashSet<usize> {
-            self.data()
-                .keys()
-                .par_bridge()
-                .cloned()
-                .collect::<HashSet<_>>()
-        }
-
-        fn get_ids(&self) -> HashSet<String> {
-            self.data()
-                .values()
-                .par_bridge()
-                .filter_map(|atom| atom.2.clone())
-                .collect::<HashSet<_>>()
-        }
-
-        fn get_classes(&self) -> HashSet<String> {
-            self.data()
-                .values()
-                .par_bridge()
-                .map(|atom| &atom.3)
-                .cloned()
-                .reduce(
-                    || HashSet::new(),
-                    |acc, next| acc.intersection(&next).cloned().collect::<HashSet<_>>(),
-                )
-        }
-
-        fn find_with_id(&self, target_id: &str) -> Option<usize> {
-            self.data().par_iter().find_map_first(|(idx, atom)| {
-                if let Some(id) = &atom.2 {
-                    if target_id == id {
-                        Some(*idx)
-                    } else {
-                        None
-                    }
-                } else {
-                    None
+        pub fn insert(&mut self, k: K, v: V) -> InsertResult<K, V> {
+            if self.validate.contains(&v) {
+                self.key_map
+                    .iter()
+                    .find_map(|(key, value)| if value == &v { Some(key) } else { None })
+                    .and_then(|key| Some(InsertResult::Duplicated(key.clone())))
+                    .unwrap()
+            } else {
+                self.validate.insert(v.clone());
+                let result = self
+                    .key_map
+                    .insert(k, v)
+                    .and_then(|v| Some(InsertResult::Updated(v)))
+                    .unwrap_or(InsertResult::Created);
+                if let InsertResult::Updated(v) = &result {
+                    self.validate.remove(v);
                 }
-            })
+                result
+            }
         }
 
-        fn find_with_classes(&self, target_classes: &HashSet<String>) -> HashSet<usize> {
-            self.data()
-                .par_iter()
-                .filter_map(|(idx, atom)| {
-                    if target_classes.is_subset(&atom.3) {
-                        Some(*idx)
-                    } else {
-                        None
-                    }
-                })
-                .collect::<HashSet<_>>()
-        }
-
-        fn get_atom(&self, idx: usize) -> Option<&Atom> {
-            self.data().get(&idx)
+        pub fn remove<Q: Hash + Eq + ?Sized>(&mut self, k: &Q) -> Option<V>
+        where
+            K: Borrow<Q>,
+        {
+            let removed = self.key_map.remove(k);
+            if let Some(value) = &removed {
+                self.validate.remove(value);
+            }
+            removed
         }
     }
-
-    impl WritableAtomLayer for AtomFillLayer {
-        fn set_atom(&mut self, idx: usize, atom: Atom) -> Option<Atom> {
-            self.data_mut().insert(idx, atom)
-        }
-    }
-
-    impl ReadableAtomLayerHelper for AtomFillLayer {}
-
-    impl WritableAtomLayerHelper for AtomFillLayer {}
-
-    pub trait ReadableBondLayer<BondT> {
-        fn get_idxs(&self) -> HashSet<Pair<usize>>;
-        fn get_value(&self, idx: &Pair<usize>) -> Option<&BondT>;
-    }
-
-    pub trait WritableBondLayer<BondT> {
-        fn set_bond(&mut self, idx: Pair<usize>, bond: BondT) -> Option<BondT>;
-
-    }
-
-    pub struct BondFillLayer<BondT = f64>(HashMap<Pair<usize>, BondT>);
-
-    impl<BondT> BondFillLayer<BondT> {
-        fn data(&self) -> &HashMap<Pair<usize>, BondT> {
-            &self.0
-        }
-
-        fn data_mut(&mut self) -> &mut HashMap<Pair<usize>, BondT> {
-            &mut self.0
-        }
-    }
-
-    impl<BondT> ReadableBondLayer<BondT> for BondFillLayer<BondT> {
-        fn get_idxs(&self) -> HashSet<Pair<usize>> {
-            self.data().keys().copied().collect::<HashSet<_>>()
-        }
-
-        fn get_value(&self, idx: &Pair<usize>) -> Option<&BondT> {
-            self.data().get(idx)
-        }
-    }
-
-    impl<BondT> WritableBondLayer<BondT> for BondFillLayer<BondT> {
-        fn set_bond(&mut self, idx: Pair<usize>, bond: BondT) -> Option<BondT> {
-            self.data_mut().insert(idx, bond)
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn uniq_val_map() {
+        let mut map: UniqueValueMap<String, usize> = UniqueValueMap::new();
+        let x = map.insert("center".to_string(), 1);
+        let y = map.insert("center".to_string(), 2);
+        let z = map.insert("entry".to_string(), 2);
+        assert_eq!(x, InsertResult::Created);
+        assert_eq!(y, InsertResult::Updated(1));
+        assert_eq!(z, InsertResult::Duplicated("center".to_string()));
+        assert_eq!(
+            map,
+            UniqueValueMap {
+                key_map: HashMap::from([("center".to_string(), 2)]),
+                validate: HashSet::from([2])
+            }
+        )
     }
 }
